b30f95e67c093edd764e42e932bfe8e4516a0555
Petey <petey.aldous@utah.edu>
2013-11-22 07:58:56 -0700

    stubbing out the code

---
+++ src/org/ucombinator/experimental/ToyParser.scala
@@ -0,0 +1,114 @@
+/*
+    Implicit Flows: a prototype taint tracking system for implicit flows
+    Copyright (C) 2013   Petey Aldous <petey.aldous@utah.edu>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package org.ucombinator.experimental
+
+import scala.util.parsing.combinator._
+import scala.language.postfixOps
+
+case class ParseError(msg: String) extends RuntimeException
+
+object ToyParser extends RegexParsers {
+
+  def labelStatement: Parser[Statement] = "(label" ~> label <~ ")" ^^ { LabelStatement(_) }
+  def gotoStatement: Parser[Statement] = "(goto" ~> label <~ ")" ^^ { GotoStatement(_) }
+  def assignStatement: Parser[Statement] = "(:=" ~> variable ~ expr <~ ")" map {
+    case variable ~ expression => AssignmentStatement(variable, expression)
+    case _ => scala.sys.error("could not match assignment statement")
+  }
+  def condStatement: Parser[Statement] = "(if" ~> expr ~ label <~ ")" map {
+    case expression ~ label => IfStatement(expression, label)
+    case _ => scala.sys.error("could not match if statement")
+  }
+  def functionCall: Parser[Statement] = "(invoke" ~> variable ~ rep(expr) <~ ")" map {
+    case variable ~ expressions => FunctionCall(variable.v, expressions)
+    case _ => throw new ParseError("Bad function call")
+  }
+  def returnStatement: Parser[Statement] = "(return" ~> expr <~ ")" ^^ { ReturnStatement(_) }
+  def throwStatement: Parser[Statement] = "(throw " ~> expr <~ ")" ^^ { ThrowStatement(_) }
+  def catchDirective: Parser[Statement] = "(catch " ~> label ~ label ~ label <~ ")" map {
+    case begin ~ end ~ handler => CatchDirective(begin, end, handler)
+    case _ => throw new ParseError("Bad catch directive")
+  }
+  def functionDeclaration: Parser[Statement] = "(fun " ~> variable ~ rep(variable) <~ ")" map {
+    case name ~ variables => FunctionDeclaration(name.v, List(variables: _*))
+    case _ => throw new ParseError("Bad function declaration")
+  }
+  def functionEnd: Parser[Statement] = "(endfun)" ^^ { _ => FunctionEnd }
+  def moveResult: Parser[Statement] = "(result " ~> variable <~ ")" ^^ { MoveResult(_) }
+
+  def label: Parser[Label] = "_[a-z]+".r ^^ { Label(_) }
+  def variable: Parser[Variable] = "[a-z]+".r ^^ { Variable(_) }
+  def expr: Parser[Expression] = addition | multiplication | comparison | variable | value
+
+  def value: Parser[Value] = "[0-9]+".r ^^ { (integer) => Value(integer.toInt) }
+  def addition: Parser[Addition] = "(+" ~> expr ~ expr <~ ")" ^^ { (result) => Addition(result._1, result._2) }
+  def multiplication: Parser[Multiplication] = "(*" ~> expr ~ expr <~ ")" ^^ { (result) => Multiplication(result._1, result._2) }
+  def comparison: Parser[Comparison] = "(=" ~> expr ~ expr <~ ")" ^^ { (result) => Comparison(result._1, result._2) }
+
+  def stmt: Parser[Statement] = (labelStatement | gotoStatement | assignStatement | condStatement
+      | functionCall | returnStatement | catchDirective | moveResult | throwStatement)
+      
+  // crashes when there are no statements - not sure why
+  // rep definitely accepts a length of 0
+  def fun: Parser[Function] = functionDeclaration ~ rep(stmt) ~ functionEnd map {
+    case decl ~ stmts ~ end => decl match {
+      case FunctionDeclaration(name, vars) => Function(name, vars, stmts)
+      case _ => throw new ParseError("Could not deconstruct FunctionDeclaration")
+    }
+    case _ => throw new ParseError("Bad function")
+  }
+  
+  def applyFuns(input: String): Map[String, Function] = {
+    def processParseResult(result: ParseResult[Function]): Map[String, Function] = result match {
+      case Success(result, remainder) => innerApplyFuns(remainder) + Pair(result.name, result)
+      case failure: NoSuccess => scala.sys.error(failure.msg)
+    }
+    def innerApplyFuns(input: Input): Map[String, Function] = {
+      if (input.atEnd) Map.empty else processParseResult(parse(fun, input))
+    }
+    processParseResult(parse(fun, input))
+  }
+
+  def applyStmt(input: String): Statement = parseAll(stmt, input) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => scala.sys.error(failure.msg)
+  }
+
+  def applyStmts(input: String): List[Statement] = {
+    def processParseResult(result: ParseResult[Statement]): List[Statement] = result match {
+      case Success(result, remainder) => result :: innerApplyStmts(remainder)
+      case failure: NoSuccess => scala.sys.error(failure.msg)
+    }
+    def innerApplyStmts(input: Input): List[Statement] = {
+      if (input.atEnd) List.empty else processParseResult(parse(stmt, input))
+    }
+    processParseResult(parse(stmt, input))
+  }
+
+  def applyExpr(input: String): Expression = parseAll(expr, input) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => scala.sys.error(failure.msg)
+  }
+
+  def applyLabel(input: String): Label = parseAll(label, input) match {
+    case Success(result, _) => result
+    case failure: NoSuccess => scala.sys.error(failure.msg)
+  }
+}
---
+++ src/org/ucombinator/experimental/Types.scala
@@ -0,0 +1,232 @@
+/*
+    Implicit Flows: a prototype taint tracking system for implicit flows
+    Copyright (C) 2013   Petey Aldous <petey.aldous@utah.edu>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+package org.ucombinator.experimental
+
+import scala.language.postfixOps
+import scala.collection.immutable.HashMap
+
+import TypeAliases._
+import scala.collection.GenTraversableOnce
+
+// TODO: make Expressions and Values and so on implement an interface for extensibility to the value system
+
+abstract class Expression {
+  def abstractMe: AbstractExpression
+}
+case class Addition(lhs: Expression, rhs: Expression) extends Expression {
+  override def abstractMe: AbstractAddition = {
+    AbstractAddition(lhs.abstractMe, rhs.abstractMe)
+  }
+}
+case class Multiplication(lhs: Expression, rhs: Expression) extends Expression {
+  override def abstractMe: AbstractMultiplication = {
+    AbstractMultiplication(lhs.abstractMe, rhs.abstractMe)
+  }
+}
+case class Comparison(lhs: Expression, rhs: Expression) extends Expression {
+  override def abstractMe: AbstractComparison = {
+    AbstractComparison(lhs.abstractMe, rhs.abstractMe)
+  }
+}
+case class Variable(v: String) extends Expression {
+  override def abstractMe: AbstractVariable = AbstractVariable(v)
+}
+case class Value(v: Int) extends Expression {
+  override def abstractMe: AbstractValue = if (v < 0) n else { if (v > 0) p else z }
+}
+
+object TypeAliases {
+  type Env = Map[Variable, Address]
+  /*
+  case class Store(val values: Map[Address, Value], val stack: Map[Address, Kontinuation]) {
+    def apply(): Store = Store(values.empty, stack.empty)
+    def apply(a: Address) = values(a)
+    def +(a: Address, v: Value): Store = new Store(values + Pair(a, v), stack)
+    def +(a: Address, k: Kontinuation): Store = new Store(values, stack + Pair(a, k))
+    def +(p: Pair[Address, Value]): Store = new Store(values + p, stack)
+    def ++(pairs: Pair[Address, Value]*): Store = new Store(values ++ pairs, stack)
+    def ++(l: List[Pair[Address, Value]]): Store = new Store(values ++ l, stack)
+  }
+  */
+  object Env {
+    def apply(): Env = Map.empty
+    def apply(v: Variable, a: Address): Env = Map(v -> a)
+    def apply(pairs: Pair[Variable, Address]*): Env = Map(pairs: _*)
+    def apply(l: List[Pair[Variable, Address]]): Env = Map(l: _*)
+  }
+  /*
+  object Store {
+    def apply(): Store = new Store(Map.empty, Map.empty)
+    def apply(a: Address, v: Value): Store = new Store(Map(a -> v), Map.empty)
+    def apply(pairs: Pair[Address, Value]*): Store = new Store(Map(pairs: _*), Map.empty)
+    def apply(l: List[Pair[Address, Value]]): Store = new Store(Map(l: _*), Map.empty)
+  }
+  */
+//  val noFunction = Function("noFunction", List.empty, List.empty)
+  case class ConcreteResult(val value: Value, val tainted: Boolean)
+  type Result = Option[ConcreteResult]
+  object Result {
+    def apply(v: Value, t: Boolean): Result = Some(ConcreteResult(v, t))
+  }
+}
+
+case class Address(a: Int)
+// no need for abstract environments because addresses stay the same
+case class Label(l: String)
+//case class Function(name: String, parameters: List[Variable], body: List[Statement])
+case class StackFrame(target: Int, previousEnv: Map[Variable, Address])
+case class AbstractStore() extends HashMap[Address, AbstractValue]
+
+/*
+class Kontinuation
+case class ConcreteKontinuation(val env: Env, val taintedVars: Set[Variable], val contextTaint: Set[Int], val f: Function, val ln: Int, val nextAddr: Address) extends Kontinuation {
+  def call(p: ConcreteProgram, s: Store, result: Result = None): ConcreteState =
+    ConcreteState(p, f, ln, env, s, s.stack(nextAddr), result, taintedVars, contextTaint)
+}
+object halt extends Kontinuation
+*/
+
+// states are (ln, env, store)
+// configurations are (state, stack summary)
+
+abstract class AbstractExpression
+case class AbstractAddition(lhs: AbstractExpression, rhs: AbstractExpression) extends AbstractExpression
+case class AbstractMultiplication(lhs: AbstractExpression, rhs: AbstractExpression) extends AbstractExpression
+case class AbstractComparison(lhs: AbstractExpression, rhs: AbstractExpression) extends AbstractExpression
+case class AbstractVariable(v: String) extends AbstractExpression
+abstract class AbstractValue() extends AbstractExpression
+
+case object nzp extends AbstractValue()
+case object nz extends AbstractValue()
+case object np extends AbstractValue()
+case object zp extends AbstractValue()
+case object n extends AbstractValue()
+case object z extends AbstractValue()
+case object p extends AbstractValue()
+
+object AbstractValues {
+  val positive = Set[AbstractValue](nzp, np, zp, p)
+  val zero = Set[AbstractValue](nzp, nz, zp, z)
+  val negative = Set[AbstractValue](nzp, nz, np, n)
+  val all = positive | zero | negative
+}
+
+abstract class Statement {
+  def isEndOfFunction: Boolean
+}
+case class LabelStatement(val l: Label) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(Label " + l + ")\""
+}
+case class AssignmentStatement(val v: Variable, val e: Expression) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(:= " + v + e + ")\""
+}
+case class GotoStatement(val l: Label) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(Goto " + l + ")\""
+}
+case class IfStatement(val condition: Expression, val l: Label) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(If " + condition + l + ")\""
+}
+case class FunctionCall(val fun: String, val exps: List[Expression]) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(" + fun + " " + exps + ")\""
+}
+case class ReturnStatement(val e: Expression) extends Statement {
+  override def isEndOfFunction: Boolean = true
+  override def toString: String = super.toString + "\"(Return " + e + ")\""
+}
+case class ThrowStatement(val e: Expression) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(throw " + e + ")\""
+}
+case class CatchDirective(val begin: Label, val end: Label, val handler: Label) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(catch " + begin + " " + end + " " + handler + ")\""
+}
+case class FunctionDeclaration(val name: String, val vars: List[Variable]) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(fun " + name + " " + vars mkString (" ") + ")\""
+}
+case object FunctionEnd extends Statement {
+  override def isEndOfFunction: Boolean = true
+  override def toString: String = super.toString + "\"(endfun)\""
+}
+case class MoveResult(val v: Variable) extends Statement {
+  override def isEndOfFunction: Boolean = false
+  override def toString: String = super.toString + "\"(result " + v + ")\""
+}
+
+case class Handler(val begin: Int, val end: Int, val code: Label) {
+  def contains(line: Int): Boolean = begin < line && end >= line
+}
+
+case class Function(val name: String, val params: List[Variable], val statements: List[Statement]) {
+  case object StaticStatementException extends RuntimeException
+
+  val labelTable = Map((statements filter { _ match { case l: LabelStatement => true; case _ => false } })
+      map { _ match { case LabelStatement(label) => (label, statements.indexOf(label)) } }: _*)
+  val catchDirectives = statements filter { _ match { case c: CatchDirective => true; case _ => false } }
+  val handlers = catchDirectives map {
+    _ match {
+      case CatchDirective(begin, end, handler) =>
+        Handler(labelTable(begin), labelTable(end), handler)
+    }
+  }
+  
+  def findExceptionHandler(line: Int): Option[Label] = {
+    handlers filter { _ contains line } match {
+      case Nil => None
+      case head :: rest => Some(head.code)
+    }
+  }
+
+  def successors(ln: Int): Set[Int] = {
+    if (isEndOfFunction(ln)) Set.empty else statements(ln) match {
+      case l: LabelStatement => Set(ln + 1)
+      case a: AssignmentStatement => Set(ln + 1)
+      case GotoStatement(l) => Set(labelTable(l))
+      case IfStatement(e, l) => Set(labelTable(l), ln + 1)
+      case _ => scala.sys.error("successors: unknown statement type")
+    }
+  }
+
+  // TODO: memoize
+  def mustReach(s: Int, seen: Set[Int] = Set.empty): Set[Int] = {
+    if (seen contains s) {
+      //      System.err.println("warning: loop. Termination leaks are possible.")
+      Set.empty
+    } else {
+      if (isEndOfFunction(s)) Set.empty else {
+        val nextSeen = seen + s
+        statements(s) match {
+          case as: AssignmentStatement => mustReach(s + 1, nextSeen) + (s + 1)
+          case ls: LabelStatement => mustReach(s + 1, nextSeen) + (s + 1)
+          case GotoStatement(l) => mustReach(labelTable(l), nextSeen) + labelTable(l)
+          case IfStatement(cond, l) => mustReach(s + 1, nextSeen) & mustReach(labelTable(l), nextSeen)
+        }
+      }
+    }
+  }
+
+  def isEndOfFunction(s: Int): Boolean = statements(s).isEndOfFunction
+}
