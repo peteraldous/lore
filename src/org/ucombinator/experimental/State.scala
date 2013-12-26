d7a15da1e3a451781903d5c05b4740be34a7ea91
Petey <petey.aldous@utah.edu>
2013-12-26 16:57:40 -0700

    next needs to return a set, as it may return multiple states

--- src/org/ucombinator/experimental/State.scala
+++ src/org/ucombinator/experimental/State.scala
@@ -4,11 +4,12 @@ import TypeAliases._
 import scala.reflect.ClassTag
 
 case object NestedFunctionException extends RuntimeException
+case object NoSuchLabelException extends RuntimeException
 
 case class State[Stored <: Value : ClassTag](val ln: Int, val f: Function, val env: Env,
   val store: Store[Stored], val taintStore: Set[Address], val contextTaint: Set[Pair[Function, Int]],
   val stack: Kontinuation) {
-  def next: State[Stored] = {
+  def next: Set[State[Stored]] = {
     def maybeAlloc(v: Variable): Env =
       if (env isDefinedAt v)
         env
@@ -16,7 +17,7 @@ case class State[Stored <: Value : ClassTag](val ln: Int, val f: Function, val e
         env + Pair(v, Analyzer.allocator.alloc(v))
     val noResultStore = store - ResultAddress
     val noResultTaintStore = taintStore - ResultAddress
-    val pass = State(ln + 1, f, env, noResultStore, noResultTaintStore, contextTaint, stack)
+    val pass = Set(State(ln + 1, f, env, noResultStore, noResultTaintStore, contextTaint, stack))
     f.statements(ln) match {
       case LabelStatement(l) => pass
       case AssignmentStatement(v, e) =>
@@ -24,8 +25,13 @@ case class State[Stored <: Value : ClassTag](val ln: Int, val f: Function, val e
         val value = Evaluator.eval[Stored](e, env, store)
         val newStore = noResultStore + Pair(newEnv(v), value)
         // TODO taint
-        State(ln + 1, f, newEnv, newStore, taintStore, contextTaint, stack)
-      case GotoStatement(l) => throw NotImplementedException
+        Set(State(ln + 1, f, newEnv, newStore, taintStore, contextTaint, stack))
+      case GotoStatement(l) =>
+        val target = f.labelTable.get(l) match {
+          case Some(i) => i
+          case None => throw NoSuchLabelException
+        }
+        Set(State(target, f, env, noResultStore, noResultTaintStore, contextTaint, stack))
       case IfStatement(condition, l) => throw NotImplementedException
       case FunctionCall(fun, exps) => throw NotImplementedException
       case ReturnStatement(e) => throw NotImplementedException
